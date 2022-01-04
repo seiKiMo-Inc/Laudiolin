b77402de9ae2294edb4140bf87d5675691141e80
KingRainbow44 <kobedo11@gmail.com>
2022-01-04 14:00:00 +0100

    Add `react-bootstrap` to the project

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
+// Import stylesheets.
 import "./App.css";
+
+import Button from "react-bootstrap/Button";
 
 interface IProps {
 
@@ -10,6 +13,11 @@ interface IState {
 
 }
 
+function clickButton() {
+    new Notification("Button", {body: "You clicked on the button!"})
+        .onclick = () => alert("You clicked on the notification!");
+}
+
 class App extends React.Component<IProps, IState> {
     render() {
         return (
@@ -17,6 +25,12 @@ class App extends React.Component<IProps, IState> {
                 <h1 className="text-3xl font-bold underline">
                     Hello world!
                 </h1>
+
+                <br />
+
+                <Button variant="primary" onClick={clickButton}>
+                    Really Cool Button!
+                </Button>
             </div>
         );
     }
