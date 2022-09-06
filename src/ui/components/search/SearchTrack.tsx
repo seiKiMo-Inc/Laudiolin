c894154643332a1880e5bfdb0b18534da457ea13
KingRainbow44 <kobedo11@gmail.com>
2022-09-06 00:33:32 -0400

    WIP search implementation

---
+++ src/ui/components/search/SearchTrack.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {SearchResult} from "backend/search";
+
+import "./SearchTrack.css";
+
+interface IProps {
+    track: SearchResult;
+}
+
+interface IState {
+
+}
+
+/* A track that appears when searching for it. */
+class SearchTrack extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    render() {
+        const track = this.props.track;
+
+        if(track) {
+            return (
+                <div id="searchTrack">
+                    <img src={track.icon} alt={track.title}/>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <a>There is no track!</a>
+            </div>
+        )
+    }
+}
+
+export default SearchTrack;
