f99b1495819829176d9a5c7c0b3c59baf2f4ec6d
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-09-07 08:14:06 +0530

    Switch `SearchResultsElement` to a Class based component

--- src/ui/components/SearchResults.tsx
+++ src/ui/components/SearchResults.tsx
@@ -2,26 +2,36 @@ import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { SearchResults } from "backend/search";
 
-const SearchResultsElement = (props: { results: SearchResults }) => {
-    return (
-        <Container style={{ marginTop: "20px" }}>
-            <div className="list-group">
-                {[props.results.top, ...props.results.results].map((result) => {
-                    return (
-                        <div className="list-group-item dark:text-white dark:bg-slate-800" key={result.id}>
-                            <Row>
-                                <Col>
-                                    <Image src={result.icon} />
-                                    <a href={result.url}>{result.title}</a>
-                                    <div className="text-gray-600">{result.artist}</div>
-                                </Col>
-                            </Row>
-                        </div>
-                    );
-                })}
-            </div>
-        </Container>
-    );
-};
+interface IProps {
+    results: SearchResults;
+}
+
+class SearchResultsElement extends React.Component<IProps, never> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Container style={{ marginTop: "20px" }}>
+                <div className="list-group">
+                    {[this.props.results.top, ...this.props.results.results].map((result) => {
+                        return (
+                            <div className="list-group-item dark:text-white dark:bg-slate-800" key={result.id}>
+                                <Row>
+                                    <Col>
+                                        <Image src={result.icon} />
+                                        <a href={result.url}>{result.title}</a>
+                                        <div className="text-gray-600">{result.artist}</div>
+                                    </Col>
+                                </Row>
+                            </div>
+                        );
+                    })}
+                </div>
+            </Container>
+        );
+    }
+}
 
 export default SearchResultsElement;
