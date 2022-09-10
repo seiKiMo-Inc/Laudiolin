9ef03bc18abf8e797cbc97e9c84391ef8b452f11
Nanako <00sh.chiha+github@gmail.com>
2022-09-10 12:15:23 +0900

    add sveltestrap

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,7 +2,7 @@
 
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "svelte";
+import { useEffect, useState } from "svelte-check";
 import { Button, FormControl, Navbar } from "sveltestrap";
 
 
