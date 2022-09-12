444e073b0d05824c2ab945a71ebea9b42ed01abc
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-09-13 01:49:00 +0530

    Add audio timestamps and styling to `ProgressBar`

--- src/ui/components/music/ProgressBar.tsx
+++ src/ui/components/music/ProgressBar.tsx
@@ -1,37 +1,57 @@
 import React from "react";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import "src/css/ProgressBar.css";
 
 interface IProps {
     progress: number;
     duration: number;
     setProgress: (value: number) => void;
 }
+
 class ProgressBarComponent extends React.Component<IProps, never> {
     constructor(props: IProps) {
         super(props);
     }
 
+    msToMinutes = (duration: number) => {
+        let minutes: number = Math.floor(duration / 60000);
+        let seconds: any = ((duration % 60000) / 1000).toFixed(0);
+        return (
+            seconds == 60 ?
+                (minutes + 1) + ":00" :
+                minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+        );
+    }
+
     render() {
         return (
-            <ProgressBar
-                className="rounded progress-bar dark:bg-gray-700 bg-slate-100 relative"
-                onClick={(e) =>
-                    this.props.setProgress(
-                        (e.nativeEvent.offsetX / e.currentTarget.offsetWidth) *
-                        this.props.duration
-                    )
+            <span id="ProgressBar">
+                {
+                /**
+                 * @todo Make the timestamps actually convert duration to minutes and seconds but I cant test since the songs dont play rn :skull:
+                 * Using the msToMinutes function above
+                 * And remove the label too ig.
+                 */
                 }
-                animated
-                now={(this.props.progress / this.props.duration) * 100}
-                label={
-                    this.props.duration > 0
-                        ? `${Math.round(this.props.progress)}/${Math.round(this.props.duration)}`
-                        : ""
-                }
-                style={{
-                    height: "10px"
-                }}
-            />
+                <p id="timestamp-start">{this.msToMinutes(0)}</p>
+                <ProgressBar
+                    className="MainProgressBar rounded progress-bar dark:bg-gray-700 bg-slate-100 relative"
+                    onClick={(e) =>
+                        this.props.setProgress(
+                            (e.nativeEvent.offsetX / e.currentTarget.offsetWidth) *
+                            this.props.duration
+                        )
+                    }
+                    animated
+                    now={(this.props.progress / this.props.duration) * 100}
+                    label={
+                        this.props.duration > 0
+                            ? `${Math.round(this.props.progress)}/${Math.round(this.props.duration)}`
+                            : ""
+                    }
+                />
+                <p id="timestamp-end">{this.msToMinutes(217612)}</p>
+            </span>
         );
     }
 }
