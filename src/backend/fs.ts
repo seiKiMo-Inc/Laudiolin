dc9b45a7db5c050093f00aafa200f610de14fa37
KingRainbow44 <kobedo11@gmail.com>
2022-09-15 00:26:55 -0400

    Data utility + prevent text selection

--- src/backend/fs.ts
+++ src/backend/fs.ts
@@ -1,5 +1,16 @@
 import type { FilePayload } from "@backend/types";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
+import { appDir, sep } from "@tauri-apps/api/path";
+
+let dataDirectory: string|null = null;
+
+/**
+ * Initializes the file system frontend handler.
+ */
+export async function initialize() {
+    // Get the data directory.
+    dataDirectory = await appDir();
+}
 
 /**
  * Returns the file path of the file.
@@ -7,4 +18,12 @@ import { convertFileSrc } from "@tauri-apps/api/tauri";
  */
 export function file(payload: FilePayload): string {
     return convertFileSrc(payload.file_path.replace(/\\"/g, '"'));
+}
+
+/**
+ * Returns the absolute path to a file in the data directory.
+ * @param path The path to the file.
+ */
+export function data(path: string): string {
+    return `${dataDirectory}${sep}${path}`;
 }
