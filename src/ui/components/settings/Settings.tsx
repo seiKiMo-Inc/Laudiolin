e9a4d3aeee26861871bc299bc34a04119e12725f
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-09-17 08:41:52 +0530

    Resolve DOM errors on Settings Menu and a bit of styling

--- src/ui/components/settings/Settings.tsx
+++ src/ui/components/settings/Settings.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 
 import * as config from "@backend/settings";
-import type {
-    SearchSettings, AudioSettings,
-    GatewaySettings, UISettings, SearchEngine
-} from "@backend/types";
+import type { AudioSettings, GatewaySettings, SearchEngine, SearchSettings, UISettings } from "@backend/types";
 
 import "@css/Settings.scss";
 import Button from "@components/Button";
@@ -43,7 +39,7 @@ class Settings extends React.Component<IProps, IState> {
                 engine: engine
             }
         });
-    }
+    };
 
     setAudio = (download_path: string) => {
         this.setState({
@@ -51,7 +47,7 @@ class Settings extends React.Component<IProps, IState> {
                 download_path: download_path
             }
         });
-    }
+    };
 
     setGateway = (encrypted?: boolean, address?: string, port?: number, gateway_port?: number) => {
         this.setState({
@@ -62,7 +58,7 @@ class Settings extends React.Component<IProps, IState> {
                 gateway_port: gateway_port
             }
         });
-    }
+    };
 
     setUi = (background_color?: string, background_url?: string) => {
         this.setState({
@@ -71,7 +67,7 @@ class Settings extends React.Component<IProps, IState> {
                 background_url: background_url
             }
         });
-    }
+    };
 
     toggleDropdown = () => {
         document.getElementById("engineDropdown").classList.toggle("show");
@@ -98,7 +94,7 @@ class Settings extends React.Component<IProps, IState> {
         if (result) {
             this.setAudio(result as string);
         }
-    }
+    };
 
     componentDidUpdate(prevProps: Readonly<IProps>, prevState: Readonly<IState>, snapshot?: any) {
         // TODO: Update settings from state.
@@ -106,119 +102,137 @@ class Settings extends React.Component<IProps, IState> {
 
     render() {
         return (
-            <>
-                <table className="SettingsOptions">
-                    <tbody>
-                        <div>
-                            <h2 className="SettingsHeadings">Search Settings</h2>
-                            <tr>
-                                <th scope="row">Accuracy:</th>
-                                <td>
-                                    <input type="checkbox" id="check"
-                                           onChange={(e) => e.currentTarget.checked ? this.setSearch(true) : this.setSearch(false)} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Engine:</th>
-                                <td>
-                                    <button onClick={this.toggleDropdown} className="dropbtn">Select Engine</button>
-                                    <div id="engineDropdown" className="dropdown-content">
-                                        <p onClick={() => this.setSearch(...[], "Youtube" as SearchEngine)}>Youtube</p>
-                                        <p onClick={() => this.setSearch(...[], "Spotify" as SearchEngine)}>Spotify</p>
-                                        <p onClick={() => this.setSearch(...[], "Soundcloud" as SearchEngine)}>Soundcloud</p>
-                                        <p onClick={() => this.setSearch(...[], "All" as SearchEngine)}>All</p>
-                                    </div>
-                                </td>
-                            </tr>
-                        </div>
-                        <div>
-                            <h2 className="SettingsHeadings">Audio Settings</h2>
-                            <tr>
-                                <th scope="row">Downloads Folder:</th>
-                                <td>
-                                    <input
-                                        className="dirInputText"
-                                        type="text"
-                                        value={this.state.audio.download_path}
-                                        readOnly
-                                    />
-                                    <Button className="dirSelector" icon={faFolder} onClick={async () => this.DirSelectorFunction()} />
-                                </td>
-                            </tr>
-                        </div>
-                        <div>
-                            <h2 className="SettingsHeadings">Gateway Settings</h2>
-                            <tr>
-                                <th scope="row">Toggle Encryption:</th>
-                                <td>
-                                    <input type="checkbox" id="check"
-                                           onChange={(e) => e.currentTarget.checked ? this.setGateway(true) : this.setGateway(false)} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Address:</th>
-                                <td>
-                                    <input
-                                        className="normalInputText"
-                                        type="text"
-                                        value={this.state.gateway.address}
-                                        onInput={(e) => this.setGateway(...[], e.currentTarget.value)}
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Port:</th>
-                                <td>
-                                    <input
-                                        className="normalInputText"
-                                        type="number"
-                                        value={this.state.gateway.port}
-                                        onInput={(e) => this.setGateway(...[], ...[], e.currentTarget.valueAsNumber)}
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Gateway Port:</th>
-                                <td>
-                                    <input
-                                        className="normalInputText"
-                                        type="number"
-                                        value={this.state.gateway.gateway_port}
-                                        onInput={(e) => this.setGateway(...[], ...[], ...[], e.currentTarget.valueAsNumber)}
-                                    />
-                                </td>
-                            </tr>
-                        </div>
-                        <div>
-                            <h2 className="SettingsHeadings">UI Settings</h2>
-                            <tr>
-                                <th scope="row">Background Color:</th>
-                                <td>
-                                    <input
-                                        className="normalInputText"
-                                        type="text"
-                                        placeholder="#000000"
-                                        value={this.state.ui.background_color}
-                                        onInput={(e) => this.setUi(e.currentTarget.value)}
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Background URL:</th>
-                                <td>
-                                    <input
-                                        className="normalInputText"
-                                        type="text"
-                                        placeholder="https://example.com/image.png"
-                                        value={this.state.ui.background_url}
-                                        onInput={(e) => this.setUi(...[], e.currentTarget.value)}
-                                    />
-                                </td>
-                            </tr>
-                        </div>
-                    </tbody>
-                </table>
-            </>
+            <table className="SettingsOptions">
+                <tbody>
+                    <tr className="SettingsHeadings">
+                        <th>Search Settings</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Accuracy:</th>
+                        <td>
+                            <input type="checkbox" id="check"
+                                   onChange={(e) => e.currentTarget.checked ? this.setSearch(true) : this.setSearch(false)} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Engine:</th>
+                        <td>
+                            <button onClick={this.toggleDropdown}
+                                    className="dropbtn">Search Engine</button>
+                            <div id="engineDropdown" className="dropdown-content">
+                                <p onClick={() => {
+                                    this.setSearch(...[], "YouTube" as SearchEngine);
+                                    this.toggleDropdown();
+                                }}>YouTube</p>
+                                <p onClick={() => {
+                                    this.setSearch(...[], "Spotify" as SearchEngine);
+                                    this.toggleDropdown();
+                                }}>Spotify</p>
+                                <p onClick={() => {
+                                    this.setSearch(...[], "SoundCloud" as SearchEngine);
+                                    this.toggleDropdown();
+                                }}>SoundCloud</p>
+                                <p onClick={() => {
+                                    this.setSearch(...[], "All" as SearchEngine);
+                                    this.toggleDropdown();
+                                }}>All</p>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr className="SettingsHeadings">
+                        <th>Audio Settings</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Downloads Folder:</th>
+                        <td>
+                            <input
+                                className="dirInputText"
+                                type="text"
+                                value={this.state.audio.download_path}
+                                readOnly
+                            />
+                            <Button className="dirSelector" icon={faFolder}
+                                    onClick={async () => this.DirSelectorFunction()} />
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr className="SettingsHeadings">
+                        <th>Gateway Settings</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Toggle Encryption:</th>
+                        <td>
+                            <input type="checkbox" id="check"
+                                   onChange={(e) => e.currentTarget.checked ? this.setGateway(true) : this.setGateway(false)} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Address:</th>
+                        <td>
+                            <input
+                                className="normalInputText"
+                                type="text"
+                                value={this.state.gateway.address}
+                                onInput={(e) => this.setGateway(...[], e.currentTarget.value)}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Port:</th>
+                        <td>
+                            <input
+                                className="normalInputText"
+                                type="number"
+                                value={this.state.gateway.port}
+                                onInput={(e) => this.setGateway(...[], ...[], e.currentTarget.valueAsNumber)}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Gateway Port:</th>
+                        <td>
+                            <input
+                                className="normalInputText"
+                                type="number"
+                                value={this.state.gateway.gateway_port}
+                                onInput={(e) => this.setGateway(...[], ...[], ...[], e.currentTarget.valueAsNumber)}
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr className="SettingsHeadings">
+                        <th>UI Settings</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Background Color:</th>
+                        <td>
+                            <input
+                                className="normalInputText"
+                                type="text"
+                                placeholder="#000000"
+                                value={this.state.ui.background_color}
+                                onInput={(e) => this.setUi(e.currentTarget.value)}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Background URL:</th>
+                        <td>
+                            <input
+                                className="normalInputText"
+                                type="text"
+                                placeholder="https://example.com/image.png"
+                                value={this.state.ui.background_url}
+                                onInput={(e) => this.setUi(...[], e.currentTarget.value)}
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         );
     }
 }
