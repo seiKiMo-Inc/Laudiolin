cb6ce4995aa360291f76c5f6abe3ce295fcc51af
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-09-19 10:15:23 +0530

    Add dropdown menu next to Search Results

--- src/ui/components/search/SearchTrack.tsx
+++ src/ui/components/search/SearchTrack.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Figure } from "react-bootstrap";
 import Button from "@components/Button";
 
-import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faPause, faPlay, faNavicon } from "@fortawesome/free-solid-svg-icons";
 import { player, playFromResult } from "@backend/audio";
 
 import type { SearchResult } from "@backend/types";
 
 import "@css/SearchTrack.scss";
+import { SearchEngine } from "@backend/types";
 
 interface IProps {
     result: SearchResult;
@@ -57,11 +58,27 @@ class SearchTrack extends React.Component<IProps, IState> {
                 this.setState({ playing: !isPlaying });
             });
         }
-    };
+    }
 
-    preview = () => {
+    preview1 = () => {
         alert("This should bring the user to a laudiolin-based song preview.");
-    };
+    }
+
+    preview2 = () => {
+        alert("Will bind functions to it when its implemented.");
+    }
+
+    openTrackSource = () => {
+        window.open(this.props.result.url, "_blank");
+    }
+
+    copyTrackURL = async () => {
+        await navigator.clipboard.writeText(this.props.result.url);
+    }
+
+    toggleDropdown = () => {
+        document.getElementById("trackDropdown").classList.toggle("show");
+    }
 
     render() {
         const result = this.props.result;
@@ -71,21 +88,47 @@ class SearchTrack extends React.Component<IProps, IState> {
                 <Figure id="figure">
                     <Figure.Caption id="statusButton">
                         <Button
+                            id="statusButtonImage"
                             icon={this.state.hasPlayed ? (this.state.playing ? faPause : faPlay) : faPlay}
                             onClick={this.playTrack}
                         />
                     </Figure.Caption>
 
-                    <a onClick={this.preview}>
+                    <a onClick={this.preview1}>
                         <Figure.Image src={result.icon} id="image" />
                     </a>
 
                     <Figure.Caption className="Title result-title">
-                        <a onClick={this.preview}>
+                        <a onClick={this.preview1}>
                             <span>{result.title}</span>
                         </a>
 
                         <p className="text-gray-600">{result.artist}</p>
+                    </Figure.Caption>
+
+                    <Figure.Caption id="searchDropdownButton">
+                        <Button
+                            icon={faNavicon}
+                            onClick={this.toggleDropdown}
+                        />
+                        <div id="trackDropdown" className="dropdown-content">
+                            <p onClick={() => {
+                                this.playTrack();
+                                this.toggleDropdown();
+                            }}>Play</p>
+                            <p onClick={() => {
+                                this.preview2();
+                                this.toggleDropdown();
+                            }}>Add to playlist</p>
+                            <p onClick={() => {
+                                this.openTrackSource();
+                                this.toggleDropdown();
+                            }}>Open track source</p>
+                            <p onClick={async () => {
+                                await this.copyTrackURL();
+                                this.toggleDropdown();
+                            }}>Copy track URL</p>
+                        </div>
                     </Figure.Caption>
                 </Figure>
             </div>
