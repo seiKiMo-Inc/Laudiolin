ef183bd520e5a383f8abff641a4b68d462ac6522
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-11-18 02:01:58 +0530

    Finish `TrackPage`

--- src/ui/pages/TrackPage.tsx
+++ src/ui/pages/TrackPage.tsx
@@ -4,6 +4,10 @@ import { TrackData } from "@backend/types";
 import { fetchTrack } from "@backend/audio";
 
 import Router from "@components/common/Router";
+import Button from "@components/common/Button";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
+
+import "@css/TrackPage.scss";
 
 interface IState {
     track: TrackData
@@ -18,12 +22,33 @@ class TrackPage extends React.Component<any, IState> {
         }
     }
 
+    openTrackSource = () => {
+        window.open(this.props.track.url, "_blank");
+    };
+
+    msToMinutes = (duration: number) => {
+        if (duration <= 0) return "--:--";
+
+        let minutes: number = Math.floor(duration / 60);
+        let seconds: any = (duration % 60).toFixed(0);
+
+        return seconds == 60 ? minutes + 1 + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    };
+
     componentDidMount() {
         fetchTrack(this.props.match.params.id).then((track) => {
             this.setState({
                 track: track
             });
         });
+
+        const root = document.getElementById("root");
+        root.style.overflow = "hidden";
+    }
+
+    componentWillUnmount() {
+        const root = document.getElementById("root");
+        root.style.overflow = "auto";
     }
 
     render() {
@@ -31,12 +56,13 @@ class TrackPage extends React.Component<any, IState> {
             return <h2 id="NoTrackMessage">No Track found.</h2>;
         }
         return (
-            <div key={this.state.track.id}>
-                <img src={this.state.track.icon} alt="Track Icon" />
-                <h2>{this.state.track.title}</h2>
-                <p>{this.state.track.artist}</p>
-                <p>{this.state.track.url}</p>
-                <p>{this.state.track.duration}</p>
+            <div className="TrackContainer" key={this.state.track.id}>
+                <div className="TrackBG" style={{ backgroundImage: `url(${this.state.track.icon})` }}></div>
+                <img className="TrackImage" src={this.state.track.icon} alt="Track Icon" />
+                <h2 className="TrackTitle">{this.state.track.title}</h2>
+                <h3 className="TrackArtist">{this.state.track.artist}</h3>
+                <h3 className="TrackDuration">{this.msToMinutes(this.state.track.duration)}</h3>
+                <Button className="TrackRedirect" icon={faShare} onClick={this.openTrackSource}>Open Source</Button>
             </div>
         )
     }
