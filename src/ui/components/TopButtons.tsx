3bca088590ecea439e8a3e0293d3c0bb44488df5
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-18 10:16:53 +0530

    Window control buttons

---
+++ src/ui/components/TopButtons.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { appWindow } from "@tauri-apps/api/window";
+import { VscClose, VscChromeMinimize, VscChromeMaximize } from "react-icons/vsc";
+
+import "@css/TopButtons.scss";
+
+class TopButtons extends React.Component {
+    closeWindow = async () => {
+        await appWindow.close();
+    }
+
+    minimizeWindow = async () => {
+        await appWindow.minimize();
+    }
+
+    maximizeWindow = async () => {
+        if (await appWindow.isMaximized()) {
+            await appWindow.unmaximize();
+        } else {
+            await appWindow.maximize();
+        }
+    }
+
+    render() {
+        return (
+            <div className={"TopButtonsContainer"}>
+                <VscClose className={"TopButtons"} onClick={this.closeWindow} />
+                <VscChromeMaximize className={"TopButtons"} onClick={this.maximizeWindow} />
+                <VscChromeMinimize className={"TopButtons"} onClick={this.minimizeWindow} />
+            </div>
+        );
+    }
+}
+
+export default TopButtons;
