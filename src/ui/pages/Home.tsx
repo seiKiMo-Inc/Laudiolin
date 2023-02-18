7c3dcc53600ecb180f3c3f72c1746e3858161679
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-18 10:41:57 +0530

    Make placeholder Home page

---
+++ src/ui/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className={"Home"}>
+                <h1>Home</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
