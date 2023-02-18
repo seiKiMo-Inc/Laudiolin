57bc3159bd28ec36609697c23aacd1f825c61641
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-18 10:13:04 +0530

    Implement theming and define app layout

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,27 +1,33 @@
 import React from "react";
 
-interface IProps {
+import TopButtons from "@components/TopButtons";
+import ControlPanel from "@components/ControlPanel";
+import NavPanel from "@components/NavPanel";
+import ContentPanel from "@components/ContentPanel";
+import TopBar from "@components/TopBar";
 
+import "@css/App.scss";
+
+interface IProps {
 }
 
 interface IState {
-
 }
 
 class App extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {
-
-        };
-    }
 
     render() {
         return (
-            <div>
-                <a>Hello world!</a>
-            </div>
+            <>
+                <TopButtons />
+                <div className={"DragBar"} data-tauri-drag-region={true} />
+                <div className={"AppContainer"}>
+                    <NavPanel />
+                    <TopBar />
+                    <ContentPanel />
+                    <ControlPanel />
+                </div>
+            </>
         );
     }
 }
