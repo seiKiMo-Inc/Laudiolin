7054b5f582dc9d84407ca6912b78919f53093f25
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-18 10:15:36 +0530

    ControlPanel component

---
+++ src/ui/components/ControlPanel.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "@css/ControlPanel.scss";
+
+class ControlPanel extends React.Component {
+    render() {
+        return (
+            <div className={"ControlPanel"}>
+                <h1>ControlPanel</h1>
+            </div>
+        );
+    }
+}
+
+export default ControlPanel;
