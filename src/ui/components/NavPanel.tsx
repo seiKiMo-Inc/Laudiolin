f7bd76503ce3e7f575e56a51fa49efc9953e9285
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-18 10:15:52 +0530

    NavPanel component

---
+++ src/ui/components/NavPanel.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "@css/NavPanel.scss";
+
+class NavPanel extends React.Component {
+    render() {
+        return (
+            <div className={"NavPanel"}>
+                <h1>NavPanel</h1>
+            </div>
+        );
+    }
+}
+
+export default NavPanel;
