421f3bb6ad7f75b469c632a165b3fd7c46a67e66
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-18 10:16:28 +0530

    TopBar component this is the search area btw

---
+++ src/ui/components/TopBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "@css/TopBar.scss";
+
+class TopBar extends React.Component {
+    render() {
+        return (
+            <div className={"TopBar"}>
+                <h1>TopBar</h1>
+            </div>
+        );
+    }
+}
+
+export default TopBar;
