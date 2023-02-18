447048d30d991386fc1edce6d7f1463492bc19b5
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-18 10:42:16 +0530

    Make placeholder SearchResults page

---
+++ src/ui/pages/SearchResults.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className={"SearchResults"}>
+                <h1>SearchResults</h1>
+            </div>
+        );
+    }
+}
+
+export default SearchResults;
