d65b18aa21011eeec10a496059a7ea0e0170b88e
KingRainbow44 <kobedo11@gmail.com>
2023-02-19 15:02:08 -0500

    Add support for React DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -29,6 +29,13 @@ import App from "./ui/App";
  * Renders the application.
  */
 function render() {
+    // Check if the application is in development mode.
+    if (process.env.NODE_ENV === "development") {
+        const reactDevTools = document.createElement("script");
+        reactDevTools.src = "http://localhost:8097";
+        document.head.appendChild(reactDevTools);
+    }
+
     // Render the application.
     const root = document.getElementById("root");
     createRoot(root!).render(<App />);
