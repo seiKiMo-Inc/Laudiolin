a9daf342f5a488a0a130627471e8039522aa9ec4
KingRainbow44 <kobedo11@gmail.com>
2023-02-18 22:31:29 -0500

    Add support for deep linking

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,47 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use tauri::{App, Wry, Manager};
+use window_shadows::set_shadow;
+
+/// Wraps Result instances into an output.
+/// Any errors are passed to the frontend.
+/// obj: The object to wrap.
+pub fn wrap<O, E>(obj: Result<O, E>, code: &str) -> O {
+    match obj {
+        Ok(output) => output,
+        Err(_) => {
+            panic!("An error occurred in the backend. {}", code);
+        }
+    }
 }
 
 fn main() {
+    // Check for an existing deep link instance.
+    tauri_plugin_deep_link::prepare("laudiolin");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            // Initialize deep linking.
+            register_deep_link(app);
+
+            // Set the window shadow.
+            let window = app.get_window("main").unwrap();
+            wrap(set_shadow(&window, true), "shadow");
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+/// Registers the deep link handler.
+fn register_deep_link(app: &mut App<Wry>) {
+    let handle = app.handle();
+    tauri_plugin_deep_link::register(
+        "laudiolin", move |request| {
+            handle.emit_all("deeplink", request).unwrap();
+        },
+    ).unwrap();
 }
---
+++ src/backend/link.ts
@@ -0,0 +1,54 @@
+import { listen, Event } from "@tauri-apps/api/event";
+
+/*
+ * Deep links:
+ * - laudiolin://play?id=
+ * - laudiolin://listen?user=
+ * - laudiolin://playlist?id=
+ */
+
+/**
+ * Sets up event listeners.
+ */
+export async function setup() {
+    console.log("Setting up link event listeners...");
+    await listen("deeplink", onLinked);
+}
+
+/**
+ * Invoked when a deep link call is received.
+ * @param event The event.
+ */
+async function onLinked(event: Event<string>) {
+    const { payload } = event;
+
+    // Validate the payload.
+    if (!payload.startsWith("laudiolin://"))
+        return;
+
+    // Parse the payload.
+    const data = payload.split(":")[1];
+    const query = data.split("/")[2];
+
+    let param = "", action = "", value = "";
+    if (payload.includes("?") && payload.includes("=")) {
+        param = payload.split("?")[1];
+        action = param.split("=")[0];
+        value = param.split("=")[1];
+    }
+
+    switch (query) {
+        default:
+            console.error("Unknown query: " + query);
+            break;
+        case "play":
+            if (action != "id") break;
+            break;
+        case "listen":
+            if (action != "id") break;
+            break;
+        case "playlist":
+            if (action != "id") break;
+            break;
+    }
+}
