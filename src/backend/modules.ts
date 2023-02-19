11ce4fbadfacff9cb223d9a3c17378796569bdec
KingRainbow44 <kobedo11@gmail.com>
2023-02-18 20:50:59 -0500

    Add types for the `player` module

--- src/backend/modules.ts
+++ src/backend/modules.ts
@@ -1,7 +1,27 @@
-export interface TrackPlayer {
+import { TrackData } from "@backend/types";
 
+export interface TrackPlayer {
+    /* Player info. */
+    getCurrentTrack(): Track | null;
+    getProgress(): number;
+    getDuration(): number;
+
+    /* Player manipulation. */
+    next(): void;
+    back(): void;
+    play(track: TrackData, force: boolean): Promise<void>;
+    stop(emit: boolean): void;
+    pause(): void;
 }
 
 export interface Track {
+    title: string;
+    artist: string;
+    icon: string;
+    url: string;
+    id: string;
 
+    clone(): Track;
+    duration(): number;
+    progress(): number;
 }
