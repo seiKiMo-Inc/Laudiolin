b46853217e471031cc4a892ecacf0ce827d6159d
KingRainbow44 <kobedo11@gmail.com>
2023-02-18 21:18:57 -0500

    Fix PostCSS error
    - Change `index.html` error
    - Change some line separators
    - Upgrade packages

--- cfg/postcss.config.js
+++ cfg/postcss.config.js
@@ -1,20 +1,25 @@
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import cssnano from "cssnano";
+import cssnanoPlugin from "cssnano";
 
 import tailwindConfig from "./tailwind.config.js";
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 
 export default {
-    plugins: [
-        // Some plugins, like TailwindCSS/Nesting, need to run before Tailwind.
-        tailwind(tailwindConfig),
+    plugins: (() => {
+        let plugins = [
+            // Some plugins, like TailwindCSS/Nesting, need to run before Tailwind.
+            tailwind(tailwindConfig),
 
-        // But others, like autoprefixer, need to run after.
-        autoprefixer(),
-        !dev && cssnano({
+            // But others, like autoprefixer, need to run after.
+            autoprefixer()
+        ];
+
+        !dev && cssnanoPlugin({
             preset: "default"
-        })
-    ]
+        });
+
+        return plugins;
+    })()
 }
