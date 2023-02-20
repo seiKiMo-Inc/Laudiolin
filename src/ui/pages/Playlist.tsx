53a317c6256f8871a3e348351b4723bf63a9395c
KingRainbow44 <kobedo11@gmail.com>
2023-02-20 11:35:55 -0500

    Implement playlists

--- src/ui/pages/Playlist.tsx
+++ src/ui/pages/Playlist.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
+import { IoMdPlay } from "react-icons/io";
+import { VscEllipsis } from "react-icons/vsc";
+import { MdFavorite, MdShuffle } from "react-icons/md";
+
+import Track from "@components/Track";
+import BasicButton from "@components/common/BasicButton";
+
 import * as types from "@backend/types";
+
+import "@css/pages/Playlist.scss";
 
 interface IProps {
     pageArgs: any;
@@ -18,8 +27,67 @@ class Playlist extends React.Component<IProps> {
 
         return (
             <div>
-                <p>Playlist</p>
-                { this.props.pageArgs && playlist.id }
+                <div className={"Playlist_Details"}>
+                    <img
+                        className={"icon"}
+                        alt={playlist.name}
+                        src={playlist.icon}
+                    />
+
+                    <div className={"info"}>
+                        <p style={{ textTransform: "uppercase", paddingTop: 28 }}>
+                            {playlist.isPrivate ? "Private Playlist" : "Public Playlist"}
+                        </p>
+
+                        <h1>{playlist.name}</h1>
+
+                        <div className={"Playlist_Author"}>
+                            <img
+                                className={"profile"}
+                                alt={playlist.owner}
+                                src={playlist.icon}
+                            />
+
+                            <p className={"username"}>{playlist.owner}</p>
+                        </div>
+
+                        <div className={"Playlist_Actions"}>
+                            <BasicButton
+                                className={"action"}
+                                icon={<IoMdPlay />}
+                            />
+
+                            <BasicButton
+                                className={"action"}
+                                icon={<MdShuffle />}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"Playlist_Interact"}>
+                    <BasicButton
+                        className={"edit"}
+                        text={"Edit Playlist"}
+                    />
+
+                    <div className={"buttons"}>
+                        <BasicButton
+                            icon={<MdFavorite />}
+                        />
+
+                        <BasicButton
+                            icon={<VscEllipsis />}
+                        />
+                    </div>
+                </div>
+
+                <div className={"Playlist_Tracks"}>
+                    {
+                        playlist.tracks.map((track, index) =>
+                            <Track track={track} key={index} />)
+                    }
+                </div>
             </div>
         );
     }
