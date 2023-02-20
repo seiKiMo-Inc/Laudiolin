971b57f457737d2fcd0b17129922dc501e6eb664
KingRainbow44 <kobedo11@gmail.com>
2023-02-20 10:21:12 -0500

    Create component & stylesheet for mini player

---
+++ src/ui/components/player/MiniPlayer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "@css/components/MiniPlayer.scss";
+
+class MiniPlayer extends React.Component<any, any> {
+    render() {
+        return (
+            <div className={"MiniPlayer"}>
+
+            </div>
+        );
+    }
+}
+
+export default MiniPlayer;
