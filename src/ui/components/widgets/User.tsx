94ad2475701503f44b6c2f399e734d1c8316ddc0
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-22 12:55:11 +0530

    Add OfflineUsers

--- src/ui/components/widgets/User.tsx
+++ src/ui/components/widgets/User.tsx
@@ -4,14 +4,15 @@ import { FiHeadphones } from "react-icons/all";
 
 import BasicButton from "@components/common/BasicButton";
 
-import type { OnlineUser } from "@backend/types";
+import type { OnlineUser, OfflineUser } from "@backend/types";
 import { listeningWith, listenWith } from "@backend/social";
 
 import "@css/components/User.scss";
 import { userData } from "@backend/user";
 
 interface IProps {
-    user: OnlineUser;
+    user: OnlineUser & OfflineUser;
+    isOffline?: boolean;
 }
 
 class User extends React.PureComponent<IProps, never> {
@@ -19,15 +20,38 @@ class User extends React.PureComponent<IProps, never> {
         super(props);
     }
 
+    formatTime(time: number) {
+        const date = new Date(time);
+        const now = new Date();
+
+        const diff = now.getTime() - date.getTime();
+        const diffYears = Math.floor(diff / (1000 * 3600 * 24 * 365));
+        const diffMonths = Math.floor(diff / (1000 * 3600 * 24 * 30));
+        const diffWeeks = Math.floor(diff / (1000 * 3600 * 24 * 7));
+        const diffDays = Math.floor(diff / (1000 * 3600 * 24));
+        const diffHours = Math.floor(diff / (1000 * 3600));
+        const diffMinutes = Math.floor(diff / (1000 * 60));
+        const diffSeconds = Math.floor(diff / (1000));
+
+        if (diffYears > 0) return `${diffYears} years ago`;
+        else if (diffMonths > 0) return `${diffMonths} months ago`;
+        else if (diffWeeks > 0) return `${diffWeeks} weeks ago`;
+        else if (diffDays > 0) return `${diffDays} days ago`;
+        else if (diffHours > 0) return `${diffHours} hours ago`;
+        else if (diffMinutes > 1) return `${diffMinutes} minutes ago`;
+        else if (diffMinutes == 1) return `${diffMinutes} minute ago`;
+        else if (diffSeconds > 0) return `${diffSeconds} seconds ago`;
+        else return "Just now";
+    }
+
     render() {
         const { user } = this.props;
-        if (user.userId == userData.userId) return undefined;
-        const listening = user.listeningTo;
+        const listening = user.listeningTo ?? user.lastListeningTo;
         if (listening == null) return undefined;
 
         return (
-            <div className={"User"}>
-                <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+            <div className={"User"} style={this.props.isOffline && { filter: "grayscale(1)", opacity: 0.6, pointerEvents: "none" }}>
+                <div style={{ display: "flex", flexDirection: "row", gap: 10, alignItems: "center" }}>
                     <img
                         className={"User_Image"}
                         src={user.avatar}
@@ -36,23 +60,31 @@ class User extends React.PureComponent<IProps, never> {
 
                     <div className={"User_Info"}>
                         <div className={"User_Name"}>
-                            {user.username}
-                            #{user.discriminator}
+                            <span>{user.username}</span>
+                            <span style={{ color: "var(--text-secondary-color)" }}>#{user.discriminator}</span>
                         </div>
-                        <p style={{ maxLines: 1 }}>{listening.title}</p>
-                        <p style={{ maxLines: 1 }}>{listening.artist}</p>
+                        <div className={"User_Listening"}>
+                            <span>{listening.title}</span>
+                            <span>{` • ${listening.artist}`}</span>
+                        </div>
                     </div>
                 </div>
 
-                <BasicButton
-                    className={"User_Listen"}
-                    icon={<FiHeadphones />}
-                    onClick={() => listeningWith != user.userId && listenWith(user.userId)}
-                    style={{
-                        backgroundColor: listeningWith == user.userId ?
-                            "var(--accent-color)" : "var(--secondary-background-color)"
-                    }}
-                />
+                {
+                    !this.props.isOffline ? (
+                        <BasicButton
+                            className={"User_Listen"}
+                            icon={<FiHeadphones />}
+                            onClick={() => listeningWith != user.userId && listenWith(user.userId)}
+                            style={{
+                                backgroundColor: listeningWith == user.userId ?
+                                    "var(--accent-color)" : "var(--secondary-background-color)"
+                            }}
+                        />
+                    ) : (
+                        <p className={"User_RecentTime"}>{this.formatTime(user.lastSeen)}</p>
+                    )
+                }
             </div>
         );
     }
--- src/ui/layout/ActivityPanel.tsx
+++ src/ui/layout/ActivityPanel.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import User from "@widget/User";
 
-import type { BasicUser } from "@backend/types";
+import type { OfflineUser, OnlineUser } from "@backend/types";
 
 import "@css/layout/ActivityPanel.scss";
-import { getAvailableUsers } from "@backend/social";
+import { getAvailableUsers, getRecentUsers } from "@backend/social";
 
 interface IState {
-    users: BasicUser[];
+    offlineUsers: OfflineUser[];
+    onlineUsers: OnlineUser[];
 }
 
 class ActivityPanel extends React.Component<{}, IState> {
@@ -16,8 +17,16 @@ class ActivityPanel extends React.Component<{}, IState> {
      * Updates the list of users.
      */
     update = async () => {
-        const users = await getAvailableUsers(true);
-        this.setState({ users });
+        const availableUsers = await getAvailableUsers();
+        const recentUsers = await getRecentUsers();
+
+        // check if the user is in both lists
+        const offlineUsers = recentUsers.filter(user => availableUsers.find(u => u.userId == user.userId) == undefined);
+
+        this.setState({
+            offlineUsers,
+            onlineUsers: availableUsers
+        })
     };
 
     interval: NodeJS.Timer|number;
@@ -26,7 +35,8 @@ class ActivityPanel extends React.Component<{}, IState> {
         super(props);
 
         this.state = {
-            users: []
+            offlineUsers: [],
+            onlineUsers: []
         };
     }
 
@@ -42,12 +52,19 @@ class ActivityPanel extends React.Component<{}, IState> {
         return (
             <div className={"ActivityPanel"}>
                 <div className={"ActivityPanel_Info"}>
-                    <h3>Now Active</h3>
+                    <h3 style={{ paddingBottom: 20 }}>Now Active</h3>
+                    <div className={"ActivityPanel_Content"}>
+                        { this.state.onlineUsers.map((user, index) =>
+                            <User user={user as OnlineUser&OfflineUser} key={index} />) }
+                    </div>
                 </div>
 
-                <div className={"ActivityPanel_Content"}>
-                    { this.state.users.map((user, index) =>
-                        <User user={user} key={index} />) }
+                <div className={"ActivityPanel_Info"}>
+                    <h3 style={{ paddingBottom: 20 }}>Recently Active</h3>
+                    <div className={"ActivityPanel_Content"}>
+                        { this.state.offlineUsers.map((user, index) =>
+                            <User user={user as OnlineUser&OfflineUser} isOffline={true} key={index} />) }
+                    </div>
                 </div>
             </div>
         );
