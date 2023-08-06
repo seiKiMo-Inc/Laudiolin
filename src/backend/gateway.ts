c21eaee9be547a86e5ae18df6eaaa4261f1215d5
KingRainbow44 <kobedo11@gmail.com>
2023-08-06 18:07:36 -0400

    Fix small gateway issues

--- src/backend/gateway.ts
+++ src/backend/gateway.ts
@@ -98,13 +98,13 @@ function onOpen(): void {
     console.info("Connected to the gateway.");
 
     // Wait for the gateway to be ready.
-    let wait = setInterval(async () => {
+    let wait = setInterval(() => {
         // Check the state of the gateway.
         if (gateway?.readyState != WebSocket.OPEN) return;
         clearInterval(wait);
 
         // Send the initialization message.
-        await sendInitMessage();
+        sendInitMessage();
         // Log gateway handshake.
         console.info("Gateway handshake complete.");
 
@@ -178,7 +178,7 @@ async function onMessage(event: MessageEvent): Promise<void> {
             emitter.emit("recent"); // Emit the recents event.
             return;
         default:
-            console.warn(message);
+            console.warn(message.message ?? "No message provided.");
             return;
     }
 }
@@ -261,6 +261,7 @@ export function listenAlongWith(userId: string | null): void {
 type BaseGatewayMessage = {
     type: string;
     timestamp: number;
+    message?: string;
 };
 
 // To server.
